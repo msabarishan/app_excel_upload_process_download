1a82cfa3559d2acbeedf9b60dc1908361527128a
msabarishan <103972490+msabarishan@users.noreply.github.com>
2022-05-16 22:33:56 +0530

    Update import_excel.py

--- import_excel.py
+++ import_excel.py
@@ -1,10 +1,85 @@
 import streamlit as st
 import pandas as pd
-
-uploaded_file = st.file_uploader("Choose a XLSX file", type="xlsx")
+import numpy as np
+st.write("""
+# Machine Allocation
+""")
+machine_priority= st.file_uploader("Choose a Machine Priority XLSX file", type="xlsx")
+machine_availability= st.file_uploader("Choose a Machine Availability XLSX file", type="xlsx")
 
 if uploaded_file:
     df = pd.read_excel(uploaded_file)
+ndf = pd.merge(mp,ma,on ='location',how ='inner')
 
-    st.dataframe(df)
-    st.table(df)
+n_p=ndf['location'].count()
+j=1
+mac_data={}
+start='maf'
+mac_data1=[start]
+d=1000
+    
+
+for i in range(n_p):
+        
+        if(d!=ndf.iloc[j-1,0]):
+            a=ndf.iloc[j-1,8]
+            b=ndf.iloc[j-1,9]
+            c=ndf.iloc[j-1,10]
+            d=ndf.iloc[j-1,0]
+    
+            
+        if (ndf.iloc[j-1,5]==1.0)and(ndf.iloc[j-1,2]<=a):
+            mac_data='ma1'
+            a=a-ndf.iloc[j-1,2]
+        
+        elif (ndf.iloc[j-1,6]==1.0)and(ndf.iloc[j-1,3]<=b):
+            mac_data='ma2'
+            b=b-ndf.iloc[j-1,3]
+        
+        elif (ndf.iloc[j-1,7]==1.0)and(ndf.iloc[j-1,4]<=c):
+            mac_data='ma3'
+            c=c-ndf.iloc[j-1,4]
+    # Above condtion to check first machine
+        elif (ndf.iloc[j-1,5]==1.0)and(ndf.iloc[j-1,2]>a)and(ndf.iloc[j-1,3]<=b)and(ndf.iloc[j-1,6]==2.0):
+            mac_data='ma2'
+            b=b-ndf.iloc[j-1,3]
+        elif (ndf.iloc[j-1,6]==1.0)and(ndf.iloc[j-1,2]>a)and(ndf.iloc[j-1,4]<=c)and(ndf.iloc[j-1,7]==2.0):
+            mac_data='ma3'
+            c=c-ndf.iloc[j-1,4]
+        
+        elif (ndf.iloc[j-1,6]==1.0)and(ndf.iloc[j-1,3]>b)and(ndf.iloc[j-1,2]<=a)and(ndf.iloc[j-1,5]==2.0):
+            mac_data='ma1'
+            a=a-ndf.iloc[j-1,2]
+        elif (ndf.iloc[j-1,6]==1.0)and(ndf.iloc[j-1,3]>b)and(ndf.iloc[j-1,4]<=c)and(ndf.iloc[j-1,7]==2.0):
+            mac_data='ma3'
+            c=c-ndf.iloc[j-1,4]
+        
+        elif (ndf.iloc[j-1,7]==1.0)and(ndf.iloc[j-1,4]>c)and(ndf.iloc[j-1,2]<=a)and(ndf.iloc[j-1,5]==2.0):
+            mac_data='ma1'
+            a=a-ndf.iloc[j-1,2]
+        elif (ndf.iloc[j-1,7]==1.0)and(ndf.iloc[j-1,4]>c)and(ndf.iloc[j-1,3]<=b)and(ndf.iloc[j-1,6]==2.0):
+            mac_data='ma2'
+            b=b-ndf.iloc[j-1,3]
+    # Above condition to check second machine
+        elif (ndf.iloc[j-1,5]==3.0)and(ndf.iloc[j-1,2]<=a):
+            mac_data='ma1'
+            a=a-ndf.iloc[j-1,2]
+        
+        elif (ndf.iloc[j-1,6]==3.0)and(ndf.iloc[j-1,3]<=b):
+            mac_data='ma2'
+            b=b-ndf.iloc[j-1,3]
+        
+        elif (ndf.iloc[j-1,7]==3.0)and(ndf.iloc[j-1,4]<=c):
+            mac_data='ma3'
+            c=c-ndf.iloc[j-1,4]
+    # Above condition to check third machine
+        
+        else:
+            mac_data='nm'
+        mac_data1.append(mac_data)
+        j=j+1
+mac_data1.remove("maf")
+ndf['Machine_allocated']=pd.DataFrame(mac_data1)
+
+st.table(ndf)
+
