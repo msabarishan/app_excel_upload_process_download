e9a6d74f6442864269ea06d9212b764a7670ca7d
msabarishan <103972490+msabarishan@users.noreply.github.com>
2022-05-16 21:41:37 +0530

    Update import_excel.py

--- import_excel.py
+++ import_excel.py
@@ -5,7 +5,7 @@ st.write("""
 # Machine_Utilization_Calculation
 """)
 
-dataUploaded=st.file_uploader(fileUploadLabel, type=['csv','xlsx'],accept_multiple_files=False,key="fileUploader")
+dataUploaded=st.file_uploader("fileUploader")
 
 try:
         df=pd.read_csv(dataUploaded, error_bad_lines=True, warn_bad_lines=False,sep=chosenFileSeparator)
