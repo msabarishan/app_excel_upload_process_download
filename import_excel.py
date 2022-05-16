9ea2978d884dca7a7840b1eac2a065cbc9871bed
msabarishan <103972490+msabarishan@users.noreply.github.com>
2022-05-16 21:57:35 +0530

    Update import_excel.py

--- import_excel.py
+++ import_excel.py
@@ -7,11 +7,7 @@ st.write("""
 
 uploaded_file = st.file_uploader("Choose a file")
 
-if uploaded_file is not None:
-        
-        df1=pd.read_csv(uploaded_file)
-        df1=pd.read_excel(uploaded_file)
-        st.dataframe(df1)
+df=pd.read_csv(uploaded_file)
         
                 
 
