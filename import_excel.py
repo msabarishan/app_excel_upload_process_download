4e0fe1775fc70dbbca838e829e880f45f1b5122c
msabarishan <103972490+msabarishan@users.noreply.github.com>
2022-05-17 19:26:43 +0530

    Update import_excel.py

--- import_excel.py
+++ import_excel.py
@@ -4,11 +4,11 @@ import numpy as np
 st.write("""
 # Machine Allocation
 """)
-df_mc_avail=pd.read_excel('mc_avail.xlsx')
-df_mc_prior=pd.read_excel('mc_prior.xlsx')
+mc_avail=pd.read_excel('mc_avail.xlsx')
+mc_prior=pd.read_excel('mc_prior.xlsx')
 
-df_mc_avail=df_mc_avail.reset_index(drop=True, inplace=True)
-df_mc_prior=df_mc_prior.reset_index(drop=True, inplace=True)
+df_mc_avail=mc_avail.reset_index(drop=True, inplace=True)
+df_mc_prior=mc_prior.reset_index(drop=True, inplace=True)
 
 
 def convert_df(machine):
